a874a3207a5743b4d784c1b654c44b9e0c013437
Eric Pauley <eric@pauley.me>
2016-11-12 18:01:34 -0500

    Added react and jsx support

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
 var path = require("path");
 module.exports = {
   entry: {
-    app: ["./app/main.js"]
+    app: ["./app/main.jsx"]
   },
   output: {
     path: path.resolve(__dirname, "build"),
     publicPath: "/assets/",
     filename: "bundle.js"
-  }
+  },
+  loaders: [
+      {
+        test: [/.jsx?$/, /.js?$/],
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
 };
