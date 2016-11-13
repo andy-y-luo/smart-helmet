bfe4e805f70875c6ad602ae2486204c3b3bc909f
Lewai A <lattia87@gmail.com>
2016-11-12 23:33:24 -0500

    line chart
    DABBING ON THEM

--- app/components/DeviceGraph.jsx
+++ app/components/DeviceGraph.jsx
@@ -1,14 +1,30 @@
 import React from "react"
+import {Chart} from "react-d3-core"
+import {LineChart} from "react-d3-basic"
+
+const x = d =>{
+  return d.time
+}
 
 const DeviceGraph = React.createClass({
-  readings() {
-    return this.props.device.readings.map((reading) => {
-      return <div>{reading.time + " - "+ reading.value}</div>
-    });
-  },
 
   render() {
-    return <div>{this.readings()}</div>
+  return <LineChart
+        showXGrid = {false}
+        showYGrid = {false}
+        title = {this.props.device.name}
+        width = {700}
+        height = {300}
+        margins = {{left:100, right: 100, top: 50, bottom:50}}
+        data = {this.props.device.readings}
+        chartSeries = {[{
+          field: "value",
+          name: "value",
+          color: '#0003ff'
+        }]}
+        x = {x}
+        />
+
   }
 })
 
